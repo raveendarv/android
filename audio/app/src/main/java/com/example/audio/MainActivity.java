a1ad74fb0fa3a1bb18550e61d3121575787aeb1b
Ravindar CR <ravindar1996@gmail.com>
2019-10-08 18:58:29 +0530

    audio

---
+++ audio/app/src/main/java/com/example/audio/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.audio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+//import android.os.RecoverySystem;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void PLAY(View v){
+        if(player==null)
+        {
+            player=MediaPlayer.create(this,R.raw.singappenne);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+
+                }
+            });
+
+        }player.start();
+    }
+    public void PAUSE(View v){
+        if(player!=null)
+        {
+            player.pause();
+        }
+    }
+    public void STOP(View v)
+    {
+        stopPlayer();
+    }
+    public void stopPlayer()
+    {
+        if(player!=null)
+        {
+            player.release();
+            player=null;
+            Toast.makeText(this,"Released",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+//    protected void onStop()
+//    {
+//        super.onStop();
+//        stopPlayer();
+//    }
+}
+
